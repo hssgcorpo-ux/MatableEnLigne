67fb4c29e505570fd4e83085b9ca30858dbf390a
hssgcorpo-ux <hssgcorpo@gmail.com>
2026-08-25 22:40:41 +0200

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// 👉 Colle ici les deux valeurs trouvées dans Supabase :
-// Project Settings → API → Project URL / anon public key
-const SUPABASE_URL = "COLLE_TON_PROJECT_URL_ICI";
-const SUPABASE_ANON_KEY = "COLLE_TA_CLE_ANON_ICI";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
